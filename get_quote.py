98f9acf45c1d5a2bba6d8ff48fe41d7ceb31e789
hallowf <alexandre.cortegaca@gmail.com>
2018-04-26 13:36:53 +0100

    Fixed merge conflict

--- get_quote.py
+++ get_quote.py
@@ -7,21 +7,23 @@ quotes_url = "http://quotes.rest/qod.json"
 def fetch_quote():
     req = requests.get(quotes_url)
     if req.status_code == 429:
-
-    parsed_response = req.json()
-    contents_obj = parsed_response["contents"]
-    quotes_obj = contents_obj["quotes"][0]
-    quote = quotes_obj["quote"]
-    return quote
+        #make request again but trough tor network
+        print("Starting tor request")
+    else:
+        parsed_response = req.json()
+        contents_obj = parsed_response["contents"]
+        quotes_obj = contents_obj["quotes"][0]
+        quote = quotes_obj["quote"]
+        return quote
 
 
 
 def convert_quote(text):
-    bin_quote = ' '.join(map(bin,bytearray(text,'utf8')))
+    bin_quote = " ".join(map(bin,bytearray(text, "utf8")))
     bin_quote = bin_quote.replace("b", "")
     return bin_quote
 
 
 
 
-print(convert_quote(fetch_quote()))
+print(convert_quote("lil wayne"))
--- tweeterBot.py
+++ tweeterBot.py
@@ -1,10 +1,11 @@
 from api_key import api
 
-bruh = "Lil wayne"
 
 def post_tweet(text):
     r = api.request('statuses/update', {'status':text})
     print(r.status_code)
 
 
-post_tweet(bruh)
+
+
+post_tweet(convert_quote(fetch_quote()))
