49ee6e09a61de19019400783aea8f170f66bcfc6
hallowf <alexandre.cortegaca@gmail.com>
2018-04-26 13:30:06 +0100

    Added api key file

---
+++ api_key.py
@@ -0,0 +1,14 @@
+from TwitterAPI import TwitterAPI
+
+consumer_key = ""
+
+consumer_secret = ""
+
+access_token_key = ""
+
+access_token_secret = ""
+
+
+
+
+api = TwitterAPI(consumer_key, consumer_secret, access_token_key, access_token_secret)
