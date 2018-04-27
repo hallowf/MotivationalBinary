410f7bfe66accbdb3d6c5f09061acfb5a26ba547
hallowf <alexandre.cortegaca@gmail.com>
2018-04-27 21:09:57 +0100

    Function to get friendship suggestions

---
+++ get_suggestions.py
@@ -0,0 +1,20 @@
+from api_key import api
+import json
+
+# Category to search users
+slug = "tecnologia"
+
+# Find suggestions of users
+def fetch_suggestions():
+    r = api.request("users/suggestions/:" + slug)
+    print(r.status_code)
+    parse_response = r.json()
+    users_inf = parse_response["users"]
+    IDS = {}
+    for x in users_inf:
+        IDS[x["id_str"]] = x["name"]
+    print(IDS)
+    return IDS
+
+
+fetch_suggestions()
