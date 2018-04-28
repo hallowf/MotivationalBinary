90d939ea7a162895a569476dd48a0fba73dc8ff3
hallowf <alexandre.cortegaca@gmail.com>
2018-04-28 13:39:51 +0100

    Put some hashswags on string

--- tweeterBot.py
+++ tweeterBot.py
@@ -2,7 +2,7 @@ from api_key import api
 from get_quote import fetch_random
 from get_quote import convert_quote
 
-binary_quote = convert_quote(fetch_random())
+binary_quote = convert_quote(fetch_random()) + " #hex #joke"
 
 def post_tweet(text):
     r = api.request("statuses/update", {"status":text})
