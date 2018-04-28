c41ab9b52cc988c3daca43c6f4c3e2b9070a8082
hallowf <alexandre.cortegaca@gmail.com>
2018-04-28 16:12:27 +0100

    Updated Bot to send Friend Requests

--- tweeterBot.py
+++ tweeterBot.py
@@ -1,12 +1,20 @@
 from api_key import api
 from get_quote import fetch_random
 from get_quote import convert_quote
+from post_friendship import make_friendship
+from post_friendship import IDS_array
 
-binary_quote = convert_quote(fetch_random()) + " #hex #joke"
+hex_quote = convert_quote(fetch_random()) + " #hex #joke"
 
 def post_tweet(text):
     r = api.request("statuses/update", {"status":text})
     print(r.status_code)
 
 
-post_tweet(binary_quote)
+def follow_request(user):
+    make_friendship(user)
+
+
+post_tweet(hex_quote)
+
+follow_request(IDS_array)
